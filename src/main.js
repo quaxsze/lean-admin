d43b826db26951f027e55162d2c37023d9be932a
quaxsze <service@plum.eu.com>
2020-07-18 20:08:05 +0200

    fix code challenge

--- src/main.js
+++ src/main.js
@@ -19,10 +19,14 @@ Vue.use(BootstrapVueIcons)
 Vue.use(VueResource)
 Vue.use(DGFAuth, {
   domain: 'dev.local:7000',
+  // domain: 'localhost:5000',
   protocol: 'http',
   // prod clientId
   clientId: '5f12b92b878eaca2dbc683f4',
   clientSecret: 'iyrusPWvrwCafxTAF9Ue6Y7xRXNpiS59CiDFff5M8pR5HuYDKs',
+  // example ID
+  // clientId: 't3pDQpnZ7EoY2Y6iDo8f4SR4',
+  // clientSecret: 'URBNainjeEnGEiageg5KMw6JqVZnLKgHOqN2KHSq3ONXSsWG',
   // demo clientId
   // clientId: '5ec80d33e7353aa3e14972e0',
   redirect: '/login'
--- src/plugins/dgf-auth.js
+++ src/plugins/dgf-auth.js
@@ -4,8 +4,8 @@ class AuthLib {
     this.http = http
     const protocol = this.options.protocol || 'https'
     this.serverURL = `${protocol}://${this.options.domain}`
-    this.state = generateRandomString()
-    this.code_verifier = generateRandomString()
+    this.state = 'f2d81a260dea8a100dd517984e53c56'
+    this.code_verifier = 'd14a30a27e77370585e078ad7cff6b139254983e3e28825ca0077f49' // Not random for dev purpose
   }
 
   _getBaseUrl () {
@@ -33,6 +33,7 @@ class AuthLib {
       {
         grant_type: 'authorization_code',
         code: code,
+        code_verifier: this.code_verifier,
         redirect_uri: `${this._getBaseUrl()}${this.options.redirect}`
       },
       {
@@ -50,13 +51,14 @@ class AuthLib {
    * Launches the login workflow
    */
   async login () {
-    // const codeChallenge = await pkceChallengeFromVerifier(this.code_verifier)
+    const codeChallenge = await pkceChallengeFromVerifier(this.code_verifier)
+    const urlCodeChallenge = encodeURIComponent(codeChallenge)
     const redirectURI = encodeURIComponent(`${this._getBaseUrl()}${this.options.redirect}`)
     const state = encodeURIComponent(this.state)
     const serverURL = `${this.serverURL}/oauth/authorize`
-    const clientId = this.options.clientId
+    const clientId = encodeURIComponent(this.options.clientId)
 
-    const urlLogin = `${serverURL}?redirect_uri=${redirectURI}&response_type=code&client_id=${clientId}&scope=default&state=${state}`
+    const urlLogin = `${serverURL}?redirect_uri=${redirectURI}&response_type=code&client_id=${clientId}&scope=default&state=${state}&code_challenge=${urlCodeChallenge}&code_challenge_method=S256`
     console.log(urlLogin)
     window.location = urlLogin
   }
@@ -99,27 +101,21 @@ const DGFAuth = {
 
 export default DGFAuth
 
-function generateRandomString () {
-  var array = new Uint32Array(28)
-  window.crypto.getRandomValues(array)
-  return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('')
+function sha256 (plain) {
+  const encoder = new TextEncoder()
+  const data = encoder.encode(plain)
+  return window.crypto.subtle.digest('SHA-256', data)
 }
 
-// function sha256 (plain) {
-//   const encoder = new TextEncoder()
-//   const data = encoder.encode(plain)
-//   return window.crypto.subtle.digest('SHA-256', data)
-// }
+function base64urlencode (str) {
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(str)))
+    .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+}
 
-// function base64urlencode (str) {
-//   return btoa(String.fromCharCode.apply(null, new Uint8Array(str)))
-//     .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
-// }
-
-// async function pkceChallengeFromVerifier (v) {
-//   const hashed = await sha256(v)
-//   return base64urlencode(hashed)
-// }
+async function pkceChallengeFromVerifier (v) {
+  const hashed = await sha256(v)
+  return base64urlencode(hashed)
+}
 
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== 'undefined' && window.Vue) {
